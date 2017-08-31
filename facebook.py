c9e7f045c14fe2cf380895602857c4073f6de834
Becca Ricks <baricks@gmail.com>
2017-08-31 18:43:09 -0400

    Scrapes only user's posts

--- facebook.py
+++ facebook.py
@@ -12,30 +12,36 @@ driver.get("http://www.facebook.com/login")
 
 # Log into Facebook
 login = driver.find_element_by_css_selector('#email')
-login.send_keys('ADD YOUR FB LOGIN HERE')
+login.send_keys('YOUR FACEBOOK LOGIN HERE')
 password = driver.find_element_by_css_selector('#pass')
-password.send_keys('ADD YOUR FB PASSWORD HERE')
+password.send_keys('YOUR PASSWORD HERE')
 password.send_keys(Keys.RETURN)
 time.sleep(2)
 
 # Go to timeline
 username = sys.argv[1]
-url = "https://www.facebook.com/" + username + "/timeline"
+url = "https://www.facebook.com/" + username + "/allactivity?privacy_source=activity_log&log_filter=cluster_11"
 driver.get(url)
 time.sleep(2)
 
 # Grab the text of each post
 
-for i in range(0,10):
+for i in range(0,30):
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+    time.sleep(1)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    timeline_posts = driver.find_elements_by_css_selector('p')
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    timeline_posts = driver.find_elements_by_xpath(".//div[@class='fsm']")
 
     for post in timeline_posts:
-        text = post.text.encode('utf-8')
-        print(text)
+        print (post.text)
         time.sleep(2)
 
 driver.quit()
